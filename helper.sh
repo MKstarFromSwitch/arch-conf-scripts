71134b31af2896e88540c6cef3859450c355065c
MKstarFromSwitch <mkstarfromswitch@gmail.com>
2026-02-24 21:24:30 +0300

    Added helper script

---
+++ helper.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+tmpdir=$(mktemp -d)
+
+cleanup() {
+  rm -rf "$tmpdir/"
+}
+trap cleanup EXIT
+
+echo "arch-conf-scripts helper"
+echo "Choose what to do:"
+echo "Note: These scripts might ask for your password, in that case just enter it."
+echo "1) Install yay (pacman for the AUR)"
+echo "2) Fix the Arch Linux keyring (to use pacman after misconfigured keyring in install)"
+read -rp "Choose an option: " option
+
+case "$option" in
+    1)
+     echo "Getting and running script..."
+     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/MKstarFromSwitch/arch-conf-scripts/HEAD/scripts/yay-install.sh)"
+     exit 0
+     ;;
+    2)
+     echo "Getting and running script..."
+     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/MKstarFromSwitch/arch-conf-scripts/HEAD/scripts/keyring-fix.sh)"
+     exit 0
+     ;;
+    *)
+     echo "Invalid choice."
+     exit 1
+     ;;
+esac
+
