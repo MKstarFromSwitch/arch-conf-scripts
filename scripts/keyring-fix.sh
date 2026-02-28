c9e748770127dc27086b531b75cc8ebcc247f3b4
MKstar <mkstarfromswitch@gmail.com>
2026-02-28 15:15:43 +0300

    Update keyring-fix.sh

--- scripts/keyring-fix.sh
+++ scripts/keyring-fix.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $EUID -eq 0 ]]; then
+  echo "Do not run this as root."
+  echo "You won't have issues, but root is unnecessary.
+  exit 1
+fi
+
 echo "Arch Linux keyring fix"
 read -p "Press ENTER to fix the keyring" -s
 echo
