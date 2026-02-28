3b4f29cf1cc8112e65bb26a770e2f8d1bc20ce09
MKstar <mkstarfromswitch@gmail.com>
2026-02-28 15:22:15 +0300

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -5,22 +5,22 @@ echo "arch-conf-scripts helper"
 echo "Choose what to do:"
 echo "Note: These scripts might ask for your password, in that case just enter it."
 echo "1) Install yay (pacman for the AUR)"
-echo "2) Fix the Arch Linux keyring (to use pacman after misconfigured keyring in install)"
+echo "2) Fix the Arch Linux keyring (useful if your pacman keyring is broken)"
 read -rp "Choose an option: " option
 
 case "$option" in
     1)
-     echo "Getting and running script..."
+     echo "Fetching and running yay installer..."
      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/MKstarFromSwitch/arch-conf-scripts/HEAD/scripts/yay-install.sh)"
      exit 0
      ;;
     2)
-     echo "Getting and running script..."
+     echo "Fetching and running keyring fix..."
      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/MKstarFromSwitch/arch-conf-scripts/HEAD/scripts/keyring-fix.sh)"
      exit 0
      ;;
     *)
-     echo "Invalid choice."
+     echo "Invalid option."
      exit 1
      ;;
 esac
