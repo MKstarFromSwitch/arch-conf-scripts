4ec06341e0f94232d45005e07e352220a24ef2ff
MKstar <mkstarfromswitch@gmail.com>
2026-02-28 15:25:51 +0300

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "arch-conf-scripts helper"
 echo "Choose what to do:"
-echo "Note: Some scripts might ask for your password, if they do just enter it."
+echo "Note: Some scripts might ask for your password, just enter it if prompted."
 echo
 echo "1) Install yay (pacman for the AUR)"
 echo "2) Fix the Arch Linux keyring (useful if your pacman keyring is broken)"
