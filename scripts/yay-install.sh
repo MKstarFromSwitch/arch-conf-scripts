3f1c103e98dad5858ab7a7238eaa449a4a12e00a
MKstar <mkstarfromswitch@gmail.com>
2026-02-28 15:11:00 +0300

    Update yay-install.sh

--- scripts/yay-install.sh
+++ scripts/yay-install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-OWNUSER="$USER"
 
 echo "Arch Linux yay installer"
 read -p "Press ENTER to install yay..." -s
@@ -11,8 +10,7 @@ sudo pacman -S --noconfirm git
 echo
 echo "Installing yay... (If it asks for your password, enter it)"
 cd ~
-sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R $OWNUSER:users yay
+git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 echo
